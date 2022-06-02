504d7fcda53a678951662df2e9e741a045435728
Max Pushkarov <maxpushka200@gmail.com>
2022-06-02 13:18:09 +0300

    feat: add GOPATH go system path var

--- run.sh
+++ run.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-# install manually git, ansible
+# 1. manually install git, zsh, ansible
+# 2. create github access token with scopes read:org and repo
+
+ansible-galaxy collection install community.general
 
 SHELL=/bin/zsh
 export GITHUB_PERSONAL_ACCESS_TOKEN=
