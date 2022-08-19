e9572992a80725381094a7ebcf720ae57e0010b2
Max Pushkarov <maxpushka200@gmail.com>
2022-08-19 12:40:56 +0300

    fix: pnpm fails to install packages

--- run.sh
+++ run.sh
@@ -3,9 +3,8 @@
 # 1. manually install git, zsh, ansible, python-pip
 # 2. create github access token with scopes `read:org` and `repo`
 
-ansible-galaxy collection install community.general
-
 SHELL=/bin/zsh
 export GITHUB_PERSONAL_ACCESS_TOKEN=
 
+ansible-galaxy collection install community.general
 ansible-playbook ./playbook.yml -K
