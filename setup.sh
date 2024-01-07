c89d67a4cf3ba6aad4cc0422fe54e86d18bc598a
Max Pushkarov <maxpushka200@gmail.com>
2024-01-07 14:10:30 +0200

    refactor: simplify setup script; reorder plays in main.yml

--- setup.sh
+++ setup.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-# 1. manually install git, zsh, ansible, python-pip
-# 2. create github access token with scopes `read:org` and `repo`
+# 1. manually install
+#    - sudo dnf install -y ansible, python3-pip, gh
 
-SHELL=/bin/zsh
-export GITHUB_PERSONAL_ACCESS_TOKEN=
+set -e # exit script on any error
+export SHELL=/bin/zsh
 
 ansible-galaxy collection install community.general
 ansible-playbook ./main.yml -K
